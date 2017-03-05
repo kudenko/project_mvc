ec5fed44ef7f3088cd1dd28d389ddaaa84f5529d
s.kudenko <ky_se_se@mail.ru>
2017-03-05 19:07:30 +0200

    added DB

--- lib/app.class.php
+++ lib/app.class.php
@@ -3,6 +3,7 @@
     class App{
 
         protected static $router;
+        public static $db;
 
         public  static function getRouter(){
             return self::$router;
@@ -10,6 +11,8 @@
 
         public static function run($uri){
             self::$router = new Router($uri);
+            self::$db = new DB(Config::get('db.host'), Config::get('db.user'),
+                Config::get('db.password'), Config::get('db.name'));
 
             Lang::load(self::$router->getLanguages());
 
